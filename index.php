8c6447922008c2f5241452eff59a175a5bb153e6
Nicolas Vidaillac <biggles414@gmail.com>
2017-12-13 04:16:17 +0100

    test1

--- index.php
+++ index.php
@@ -16,13 +16,11 @@ if($method == 'POST'){
 
     switch ($text) {
         case 'hi':
-            $speech = "<iframe
-  width=\"600\"
-  height=\"450\"
-  frameborder=\"0\" style=\"border:0\"
-  src=\"https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY
-    &q=Space+Needle,Seattle+WA\" allowfullscreen>
-</iframe>";
+            $speech = "hello";
+            break;
+
+        case 'map':
+            $speech = "http://google.fr";
             break;
 
         case 'bye':
